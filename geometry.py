2ebe1686c971c0c8f84ad6cd6c7249549d672fdb
farrukhsahi <153014634+farrukhsahi@users.noreply.github.com>
2024-05-20 13:53:31 -0400

    Add files via upload

---
+++ geometry.py
@@ -0,0 +1,8 @@
+def square_footage(length, width):
+    """Calculate the square footage of a house"""
+    return length * width
+
+def circumference(radius):
+    """Calculate the circumference of a circle"""
+    import math
+    return 2 * math.pi * radius
